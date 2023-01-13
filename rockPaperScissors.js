8f1483b9630171d7c548378223ac8ccddb6ae768
JivkoKarakashev <d1v1n3.85@gmail.com>
2023-01-13 17:37:24 +0200

    Update rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,57 +1,65 @@
-function rockPaperScissors(){
+function rockPaperScissors() {
+    const chalk = require('chalk');
 
     const readline = require('readline').createInterface({
         input: process.stdin,
         output: process.stdout
     })
-    
+
     const rock = "Rock";
     const paper = "Paper";
     const scissors = "Scissors";
 
     let computerRandomNumber = Math.floor(Math.random() * 3) + 1;
-    let computerTurn;    
+    let computerTurn;
     let playerTurn;
     
-    let recursiveAsyncReadLine = function () {        
-        readline.question(`Choose one of the following three options:\n          -1.Type "r" OR rock" in console, if your choice is "Rock";\n          -2.Type "p" OR "paper" in console, if your choice is "Paper"\n          -3."Type "s" OR "scissors" in console, if your choice is ""Scissors:`, string => {
-            
+    let recursiveAsyncReadLine = function () {
+        let isInvalid = false;
+        readline.question(`Choose one of the following three options:
+        1.Type "r" OR rock" in console, if your choice is "Rock";
+        2.Type "p" OR "paper" in console, if your choice is "Paper"
+        3.Type "s" OR "scissors" in console, if your choice is "Scissors"\n`, string => {
+
             playerTurn = String(string);
-            
-            if (playerTurn == "r" || playerTurn == "rock"){
+
+            if (playerTurn == "r" || playerTurn == "rock") {
                 playerTurn = rock;
-            } else if (playerTurn == "p" || playerTurn == "paper"){
+            } else if (playerTurn == "p" || playerTurn == "paper") {
                 playerTurn = paper;
-            } else if (playerTurn == "s" || playerTurn == "scissors"){
+            } else if (playerTurn == "s" || playerTurn == "scissors") {
                 playerTurn = scissors;
             } else {
-                console.log("Invalid input. Try Again...")
+                isInvalid = true;
+                console.log(chalk.bgRedBright("Invalid input. Try Again..."))
                 recursiveAsyncReadLine()
             }
-            console.log(`You choose ${playerTurn}`)
+            if (!isInvalid) {
+                console.log(chalk.bgWhiteBright(`You choose ${playerTurn}`))
 
-            switch (computerRandomNumber){
-                case 1: computerTurn = rock; break;
-                case 2: computerTurn = paper; break;
-                case 3: computerTurn = scissors; break;
-                default: ; break;
-            }
-            console.log(`The computer chooses ${computerTurn}`)
+                switch (computerRandomNumber) {
+                    case 1: computerTurn = rock; break;
+                    case 2: computerTurn = paper; break;
+                    case 3: computerTurn = scissors; break;
+                    default: ; break;
+                }
+                console.log(chalk.bgWhiteBright(`The computer chooses ${computerTurn}`))
 
-            if ((playerTurn === rock && computerTurn === scissors)
-                || (playerTurn === paper && computerTurn === rock)
-                || (playerTurn === scissors && computerTurn === paper)){
-                    console.log("You win!")
+                if ((playerTurn === rock && computerTurn === scissors)
+                    || (playerTurn === paper && computerTurn === rock)
+                    || (playerTurn === scissors && computerTurn === paper)) {
+                    console.log(chalk.bgGreen("You win!"))
                     return readline.close()
                 } else if ((playerTurn === rock && computerTurn === paper)
-                || (playerTurn === paper && computerTurn === scissors)
-                || (playerTurn === scissors && computerTurn === rock)){
-                    console.log("You lose!")
+                    || (playerTurn === paper && computerTurn === scissors)
+                    || (playerTurn === scissors && computerTurn === rock)) {
+                    console.log(chalk.bgRed("You lose!"))
                     return readline.close()
                 } else {
-                    console.log("This game was a draw!")
+                    console.log(chalk.bgYellow("This game was a draw!"))
                     return readline.close()
                 }
+            }
         })
     }
     recursiveAsyncReadLine()
